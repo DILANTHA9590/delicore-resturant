918f73c568b53c6683a144fa80b2818446671eb2
dilanthanayanajith <dilanthanayanajith@gmail.com>
2025-11-05 22:34:36 +0530

    add homae page background image

--- src/homeLayout/pages/HomePage.jsx
+++ src/homeLayout/pages/HomePage.jsx
@@ -3,8 +3,17 @@ import React from "react";
 export default function HomePage() {
   return (
     <>
-      <section className="h-full">
-        <div className="h-full bg-accent"></div>
+      <section
+        className="h-full "
+        style={{
+          backgroundImage:
+            "url('https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msIAu.img?encodeType=webp')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+        }}
+      >
+        {/* <div className="fixed inset-0 bg-black/90 w-"></div> */}
       </section>
     </>
   );
