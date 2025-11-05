f9ee74bff88d121eb5c90439bffdb14df296b194
dilanthanayanajith <dilanthanayanajith@gmail.com>
2025-11-05 22:36:13 +0530

    creat comepenent for side nav br

--- src/homeLayout/HomeLayOut.jsx
+++ src/homeLayout/HomeLayOut.jsx
@@ -1,43 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import HomePage from "./pages/HomePage";
 import { Link, Route, Routes } from "react-router-dom";
+import SideNavBar from "./component/SideNavBar";
+import { motion } from "framer-motion";
 
 export default function HomeLayout() {
+  const [openNav, setOpenBav] = useState(true);
+
   return (
     <>
-      <div
-        className="h-screen overflow-hidden overflow-y-auto"
-        style={{
-          backgroundImage:
-            "url('https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msIAu.img?encodeType=webp')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "100vh",
-        }}
-      >
-        <div className="flex flex-col h-full sm:px-8">
-          <div className="h-[10%] opacity-55 backdrop-blur-xl flex  justify-between items-center sm:text-4xl text-description font-bold ">
-            <div className="leading-4 sm:leading-8">
-              <h1>Delicore</h1>
-              <h1 className="pl-5 sm:pl-10">Resturant</h1>
-            </div>
-            <div className="flex items-center gap-3 sm:gap-10">
-              <div className="flex gap-3 sm:gap-10">
-                <Link>Login</Link>
-                <Link clas>Register</Link>
-              </div>
-              <div>
-                <div className="flex flex-col gap-2">
-                  <h1 className="w-13 h-1.5 bg-amber-50"></h1>
-                  <h1 className="w-13 h-1.5 bg-amber-50"></h1>
-                  <h1 className="w-13 h-1.5 bg-amber-50"></h1>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="h-screen overflow-hidden overflow-y-auto">
+        <motion.div className="fixed inset-0 bg-black/90 w-[20%] "></motion.div>
 
-          <div className="h-[90%]">
+        <div className="flex flex-col h-full ">
+          <SideNavBar setOpenBav={setOpenBav} openNav={openNav} />
+          {/* 
+          <div className="fixed inset-0 bg-black/90 w-[20%]  "></div> */}
+          <div className="h-[90%] ">
             <Routes>
               <Route path="/" element={<HomePage />} />
             </Routes>
